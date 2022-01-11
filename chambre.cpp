eba037157ad1fd19d344c32652578b8974b4388d
Quentin-Filleule <65295378+Quentin-Filleule@users.noreply.github.com>
2022-01-11 11:25:16 +0100

    Class chambre

---
+++ chambre.cpp
@@ -0,0 +1,32 @@
+#include "chambre.h"
+#include <iostream>
+
+Chambre::Chambre(int num,std::string type, int prix): _num(num), _type(type), _prix(prix) {
+
+}
+
+int Chambre::getNum() const{
+
+    return _num;
+}
+
+std::string Chambre::getType() const {
+
+    return _type;
+}
+
+int Chambre::getPrix() const {
+
+    return _prix;
+}
+
+void Chambre::setPrix(double new_prix)
+{
+    _prix=new_prix;
+}
+
+std::ostream& operator<<(std::ostream& os, const Chambre& client){
+    os << client.getNum() << "-" << client.getType() << "-" << client.getPrix() << std::endl;
+    return os;
+    
+}
---
+++ chambre.h
@@ -0,0 +1,22 @@
+#include <string>
+
+
+class Chambre
+{
+private :
+    int _num;
+    int _prix;
+    std::string _type;
+   
+
+public :
+    Chambre(int num,std::string type,int prix);
+    int getNum() const;
+    int getPrix() const;
+    void setPrix(double new_prix);
+    std::string getType() const;
+    
+
+};
+
+std::ostream& operator<<(std::ostream& os, const Chambre& client);
